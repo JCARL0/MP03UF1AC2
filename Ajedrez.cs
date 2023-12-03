ad9f63f67d510a5834f4a3c945b6a626332ce330
JCR <147548026+JCARL0@users.noreply.github.com>
2023-12-03 16:47:21 +0100

    Ejer7 - Ajedrez 8x8 B/N

---
+++ Ajedrez.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int tamanoTablero = 8;
+
+        for (int fila = 0; fila < tamanoTablero; fila++)
+        {
+            for (int columna = 0; columna < tamanoTablero; columna++)
+            {
+                if (fila % 2 == 0)
+                {
+                    Console.Write(columna % 2 == 0 ? "B" : "N");
+                }
+                else
+                {
+                    Console.Write(columna % 2 == 0 ? "N" : "B");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
