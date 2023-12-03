e85256faf59d9d8143f876b985a5992d61006f61
JCR <147548026+JCARL0@users.noreply.github.com>
2023-12-03 16:37:42 +0100

    Ejer5 - DNI compatible con la letra A

--- DNI.cs
+++ DNI.cs
@@ -3,17 +3,35 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Entroduce el numero del DNI: ");
-        int numero = int.Parse(Console.ReadLine());
-        char letra = CalcularLetraDNI(numero);
-        Console.WriteLine($"La letra del DNI es: {letra}");
+        char letraObjetivo = 'A';
+
+        Console.WriteLine($"Introduce numeros del DNI hasta que se identica a la letra '{letraObjetivo}'");
+
+        while (true)
+        {
+            Console.Write("Introduce el numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            char letraCalculada = CalcularLetraDNI(numero);
+
+            if (letraCalculada == letraObjetivo)
+            {
+                Console.WriteLine($"Numero {numero} del DNI es compatible con la letra '{letraObjetivo}'");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Numero {numero} swl DNI no es compatible");
+            }
+        }
     }
 
     static char CalcularLetraDNI(int numero)
     {
-        char[] letras = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+        char[] letrasDNI = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+
         int residuo = numero % 23;
-        char letra = letras[residuo];
-        return letra;
+
+        return letrasDNI[residuo];
     }
 }
