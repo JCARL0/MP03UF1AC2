299f8a61fb16d21ece0c573107d4fb3f564c0477
JCR <147548026+JCARL0@users.noreply.github.com>
2023-12-03 16:27:26 +0100

    Ejer4 - Letra del DNI

---
+++ DNI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Entroduce el numero del DNI: ");
+        int numero = int.Parse(Console.ReadLine());
+        char letra = CalcularLetraDNI(numero);
+        Console.WriteLine($"La letra del DNI es: {letra}");
+    }
+
+    static char CalcularLetraDNI(int numero)
+    {
+        char[] letras = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+        int residuo = numero % 23;
+        char letra = letras[residuo];
+        return letra;
+    }
+}
